3111c1ae0be46d94fcf4dbafed736107866c1cc1
SortOf430 <sellostefano@gmail.com>
2018-09-21 13:01:52 +0200

    fixed user header

--- header.php
+++ header.php
@@ -26,7 +26,26 @@
                         </li>
                         <li>
                         <?php 
-                            if(isset($_SESSION['user'])){ ?>
+                            if(isset($_SESSION['user'])){ 
+                                //user query in order to take name/surname
+                                //user selection query
+                                $user_query = 'SELECT * FROM Utenti where Mail=?';
+
+                                //fetch user
+                                $result = $conn->prepare($user_query);
+                                $result->execute([$_SESSION['user']]);
+                                $user = $result->fetch();
+                                $user_name = $user['Nome'];
+                                $user_surname = $user['Cognome'];
+                                $user_mail = $user['Mail'];
+                                $user_psw = $user['Password'];
+                                $user_salt = $user['Salt'];
+                                $admin = $user['Amministratore'];
+
+                                //if not admin, redirect to login
+                                if ($admin) {
+                                    header('Location: admin/index.php');
+                                }?>
                             <a class="dropdown-toggle" data-toggle="dropdown" href="#">
                                 <?php echo (strtoupper($user_name . ' ' . $user_surname . ' ')); ?> <i class="fa fa-angle-down"></i>
                             </a>
