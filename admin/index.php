f4959fdc3f1074dbd210bae368e52dc5d53121e2
Stefano Sello <sellostefano@gmail.com>
2018-10-28 23:00:15 +0100

    little fix

--- admin/index.php
+++ admin/index.php
@@ -86,7 +86,7 @@ $movies = $conn->query($movie_query);
                 </div>
                 <?php }?>
                 
-                <?php if (isset($_SESSION['update-user-success']) && $_SESSION['update-profile-success']) {?>
+                <?php if (isset($_SESSION['update-user-success']) && $_SESSION['update-user-success']) {?>
                 <div class="alert alert-success" >
                         <strong>SUCCESS :</strong> <?php echo($_SESSION['update-user-success']);?>'s information updated successfully! :)
                 </div>
